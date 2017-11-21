0b07fcba7dfbe3824bc8bd36c7a3587112bf99ec
Glen Burchfield <33846441+gburchfield@users.noreply.github.com>
2017-11-21 17:53:52 -0500

    Add Updates 11/21
    Add Content to Skill and About section
    Add Career section
    Add Footer section

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,20 @@
 //SERVICES
 
 $(function () {
+    //animate on scroll
     new WOW().init();
+});
+
+//CAREER
+
+$(function(){
+    $(".open-popup-link").magnificPopup(
+        {
+            midClick: true,
+            type: "inline",
+            gallery:{
+                enabled:true
+            }
+        }
+    );
 });
