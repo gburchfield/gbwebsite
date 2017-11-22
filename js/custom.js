1487eabcf8a9cd064528d1b3de20329b5893ef9d
Glen Burchfield <33846441+gburchfield@users.noreply.github.com>
2017-11-21 22:53:15 -0500

    Add files via upload
    Add Navigation
    Add Scroll Effects

--- js/custom.js
+++ js/custom.js
@@ -18,3 +18,28 @@ $(function(){
         }
     );
 });
+
+//NAVIGATION
+
+$(function () {
+    $(window).scroll(function () {
+        if($(this).scrollTop() < 100){
+            $("nav").removeClass("glen-top-nav")
+        }
+        else {
+            $("nav").addClass("glen-top-nav")
+        }
+    })
+});
+
+//SCROLL
+
+$(function () {
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        var section = $(this).attr("href");
+        $("html,body").animate({
+            scrollTop: $(section).offset().top - 65
+        }, 1250, "easeInOutExpo");
+    })
+});
