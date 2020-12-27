e7b673820591b1d3266c7d06ae956adefaa3fb6d
Glen <Glen.Burchfield.91@gmail.com>
2020-12-27 15:58:15 -0500

    Add Bootstrap, Google fonts, custom global css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
 module.expoerts = {
-    plugins: ["gatsby-plugin-react-helmet"]
+    siteMetadata: {
+      title: `Glen Burchfield`,
+        siteUrl: `https://glenburchfield.com`,
+        description: `Glen Burchfield is a Software Solutions Architect for Unsuccessful Technologies LLC.  Take a look at his skills, experience and projects.`
+    },
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [`Roboto+Condensed:300,300i,400,400i,700,700i`],
+                display: `swap`
+            }
+        }
+    ]
 }
